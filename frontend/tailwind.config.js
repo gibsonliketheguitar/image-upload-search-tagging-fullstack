d386284cbfbbba2d2c81f405d416d9cd458ac746
Gibson Han <gibsonliketheguitar2@gmail.com>
2022-12-20 19:52:38 -0800

    update tailwind content path configuration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,10 +2,11 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./component/**/*.{js,ts,jsx,tsx}",
+    "./core-element/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
