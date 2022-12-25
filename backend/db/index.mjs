d123a083a4fc75885ac41fc2a077268cfac30cdd
Gibson Han <gibsonliketheguitar2@gmail.com>
2022-12-24 22:41:01 -0800

    add env for front and backend

--- backend/db/index.mjs
+++ backend/db/index.mjs
@@ -1,18 +1,20 @@
 import pg from 'pg';
 
-const credentials = {
-    user: process.env.DB_USER || "postgres",
-    host: process.env.HOST || "localhost",
-    database: process.env.DB || "images",
-    password: process.env.DB_PW || "9359",
-    port: process.env.DB_PORT || 5432,
-};
+
 
 // Connect with a connection pool.
 
 const { Pool, Client } = pg
 
 export default async function db() {
+    const credentials = {
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PW,
+        port: process.env.DB_PORT
+    };
+
     const client = new Client(credentials)
     return client
 }
--- backend/service/createTags.js
+++ backend/service/createTags.js
@@ -1,14 +1,11 @@
-
 import got from "got";
-
-
 
 export default async function createTags(key) {
     const awsS3URL = `https://s3-chainsawman-bucket.s3.us-west-1.amazonaws.com/${key}`
     const url = 'https://api.imagga.com/v2/tags?image_url=' + encodeURIComponent(awsS3URL);
 
     try {
-        const response = await got(url, { username: apiKey, password: apiSecret });
+        const response = await got(url, { username: process.env.IMAGGA_API_KEY, password: process.env.IMAGGA_SECRET });
         return await JSON.parse(response.body)
     } catch (error) {
         console.log('what is error', error)
